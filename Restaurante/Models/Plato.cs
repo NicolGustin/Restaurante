83ca4c1550e65e44e6486d8ae1fa4f3eec45c1f8
Nicol Gustin <nicol930519@hotmail.com>
2024-05-23 20:28:01 -0500

    Plato

---
+++ Restaurante/Models/Plato.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurante.Models
+{
+    public class Plato
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public string Descripción { get; set; }
+    }
+}
