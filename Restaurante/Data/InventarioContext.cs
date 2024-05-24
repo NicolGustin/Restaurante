07c56a85d0b366f50e895878c52669313fa8424a
Nicol Gustin <nicol930519@hotmail.com>
2024-05-23 21:21:49 -0500

    Data

---
+++ Restaurante/Data/InventarioContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurante.Models;
+
+namespace Restaurante.Data
+{
+    public class InventarioContext
+    {
+        public DbSet<Ingrediente> Ingredientes { get; set; }
+        public DbSet<Plato> Platos { get; set; }
+        public DbSet<DetallePlato> DetallesPlato { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+
+    }
+}
