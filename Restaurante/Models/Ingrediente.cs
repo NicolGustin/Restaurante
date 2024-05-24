c73c598df474a568b52dcd6b98509afcc3ab5ba4
Nicol Gustin <nicol930519@hotmail.com>
2024-05-23 20:24:56 -0500

    Ingredientes Models

---
+++ Restaurante/Models/Ingrediente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurante.Models
+{
+    public class Ingrediente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        [Column(TypeName = "decimal(6,2)")]
+        public double CantidadDisponible { get; set; }
+        public string UnidadMedida { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int DetallePlato { get; set; }
+        public required DetallePlato DetallePlato { get; set; }
+    }
+}
