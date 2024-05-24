fd7cfdfb8801c42c59d225512ec0ed38eb2d8d68
Nicol Gustin <nicol930519@hotmail.com>
2024-05-23 20:29:48 -0500

    DetallePlato Models

---
+++ Restaurante/Models/DetallePlato.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurante.Models
+{
+    public class DetallePlato
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public ICollection<DetallePlato>? DetallePlatos { get; set; }
+    }
+}
