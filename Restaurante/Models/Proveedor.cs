549dfae4e20b3a53888ebeced8a8f3ced5da24ca
Nicol Gustin <nicol930519@hotmail.com>
2024-05-23 20:32:56 -0500

    Proveedor Models

---
+++ Restaurante/Models/Proveedor.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurante.Models
+{
+    public class Proveedor
+    {
+        public int IdProveedor { get; set; }
+        public string NombreEmpresa { get; set; }
+        public string Contacto { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public ICollection<Compra>? Compras { get; set; }
+    }
+}
