292648e7ce276cccf2db75b513885276c8f20a54
Nicol Gustin <nicol930519@hotmail.com>
2024-05-24 21:51:34 -0500

    Index Ingrediente

---
+++ Restaurante/Pages/Ingredientes/Create.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurante.Data;
+using Restaurante.Models;
+
+namespace Restaurante.Pages.Ingredientes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly InventarioContext _context;
+
+        public CreateModel(InventarioContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+
+        public Ingrediente Ingrediente { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Ingredientes == null || Ingrediente == null)
+            {
+                return Page();
+            }
+
+            _context.Ingredientes.Add(Ingrediente);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+}
